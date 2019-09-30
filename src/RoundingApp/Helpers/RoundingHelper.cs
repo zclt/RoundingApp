b65d128c5a05198eda03a2edfbf6af985568f3f3
Matheus Antonio Zucolotto Pereira <matheus.pereira@promob.com>
2019-09-30 09:59:08 -0300

    Commit

---
+++ src/RoundingApp/Helpers/RoundingHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoundingApp.Helpers
+{
+    public class RoundingHelper
+    {
+        public static int _defaultPrecision = 4;
+
+        public static decimal Round(decimal value, int precision)
+        {
+            return Math.Round(value, precision, MidpointRounding.AwayFromZero);
+        }
+
+        public static decimal Round(decimal value)
+        {
+            return Round(value, _defaultPrecision);
+        }
+    }
+}
---
+++ src/RoundingAppTest/Helpers/RoundingHelperTest.cs
@@ -0,0 +1,64 @@
+﻿using RoundingApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RoundingAppTest.Helpers
+{
+    public class RoundingHelperTest
+    {
+        [Fact]
+        public void RoundTest()
+        {
+            //arrange
+            var value = 169.5658m;
+
+            //act
+            var roundedValue = RoundingHelper.Round(value);
+
+            //assert
+            Assert.Equal(169.5658m, roundedValue);
+        }
+
+        [Fact]
+        public void Round2Test()
+        {
+            //arrange
+            var value = 169.56585m;
+
+            //act
+            var roundedValue = RoundingHelper.Round(value);
+
+            //assert
+            Assert.Equal(169.5659m, roundedValue);
+        }
+
+        [Fact]
+        public void Round3Test()
+        {
+            //arrange
+            var value = 169.56585m;
+
+            //act
+            var roundedValue = RoundingHelper.Round(value, 2);
+
+            //assert
+            Assert.Equal(169.57m, roundedValue);
+        }
+
+        [Theory]
+        [InlineData(12.555, 12.56)]
+        [InlineData(12.565, 12.57)]
+        [InlineData(12.554, 12.55)]
+        [InlineData(12.558, 12.56)]
+        public void RoundingsTest(decimal value, decimal rounded)
+        {
+            //arrange & act
+            var result = RoundingHelper.Round(value, 2);
+
+            //assert
+            Assert.Equal(rounded, result);
+        }
+    }
+}
